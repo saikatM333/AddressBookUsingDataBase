1158c19aca3528b621427dfffff27119499d8d16
saikatCV <saikat030703@gmail.com>
2024-04-23 19:59:18 +0530

    added the client class

--- AddressBookUsingDataBase/AddressBookClient.cs
+++ AddressBookUsingDataBase/AddressBookClient.cs
@@ -26,31 +26,32 @@ namespace AddressBookUsingDataBase
 
         public void Run()
         {
-            //_CreateAddressBook.CreateTables("AddressBooks");
-            //_CreateContactDeatils.CreateTables("ContactDetails");
+            _CreateAddressBook.CreateTables("AddressBooks");
+            _CreateContactDeatils.CreateTables("ContactDetails");
 
-            //// Add address books
-            //_addData.AddAddressBook("Personal"); 
-            //_addData.AddAddressBook("Work");
+            // Add address books
+            _addData.AddAddressBook("Personal");
+            _addData.AddAddressBook("Work");
 
-            //// Add contacts
-            //var contact1 = new SingleContact("John Doe", "john@example.com", "1234567890", "New York", "NY", "10001" );
-            //var contact2 = new SingleContact("Jane Smith", "jane@example.com", "9876543210", "Los Angeles", "CA", "90001" );
-            //_addData.AddContact(contact1 , 1);
-            // _addData.AddContact(contact2,1);
+            // Add contacts
+            var contact1 = new SingleContact("John Doe", "john@example.com", "1234567890", "New York", "NY", "10001");
+            var contact2 = new SingleContact("Jane Smith", "jane@example.com", "9876543210", "Los Angeles", "CA", "90001");
+            _addData.AddContact(contact1, 1);
+            _addData.AddContact(contact2, 1);
 
-            // Display address books and contacts
+           // Display address books and contacts
             Console.WriteLine("Address Books:");
-            //_fetchData.DisplayAddressBooks();
+            _fetchData.DisplayAddressBooks();
 
-            // update details 
-            //_updateDetails.UpdateContacts("name", "saikat", "John Doe");
+           // update details
+            _updateDetails.UpdateContacts("name", "saikat", "John Doe");
 
             _fetchData.DisplayAddressBooks();
-            //_deleteDetails.DeleteContacts("Jane Smith");
-            // _fetchData.DisplayAddressBooks();
-            //Console.WriteLine("\nContacts:");
-            //_fetchData.DisplayContacts();
+            _deleteDetails.DeleteContacts("Jane Smith");
+            _fetchData.DisplayAddressBooks();
+            Console.WriteLine("\nContacts:");
+            _fetchData.DisplayAddressBooks();
+
         }
     }
 }
