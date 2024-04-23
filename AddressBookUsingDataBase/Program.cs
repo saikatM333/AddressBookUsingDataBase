2f0ec740e35bf75f1a000f19cb1458ada8195e33
saikatCV <saikat030703@gmail.com>
2024-04-23 20:02:38 +0530

    added object to run the application

--- AddressBookUsingDataBase/Program.cs
+++ AddressBookUsingDataBase/Program.cs
@@ -1,7 +1,22 @@
-﻿public  class Program
+﻿using AddressBookUsingDataBase;
+
+public  class Program
 {
     public  static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        Console.WriteLine("Address Book");
+
+        string connectionString = "server=(localdb)\\MSSQLLOCALDB; Initial Catalog = addressBook; Integrated Security = SSPI";
+        Connection connection = new Connection(connectionString);
+
+        CreateContactDeatils createContactDeatils = new CreateContactDeatils(connection);
+        CreateAddressBook createAddressBook = new CreateAddressBook(connection);
+        AddData addData = new AddData(connection);
+        FetchData fetchData = new FetchData(connection);
+        UpdateDetails updateDetails = new UpdateDetails(connection);
+        DeleteDetails deleteDetails = new DeleteDetails(connection);
+
+        AddressBookClient client = new AddressBookClient(createContactDeatils, createAddressBook, addData, fetchData, updateDetails, deleteDetails);
+        client.Run(); Console.WriteLine("Hello, World!");
     }
 }
